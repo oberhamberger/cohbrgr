03e8c33459f4da8a071ac4f317e40a358a51f556
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2021-08-02 20:44:01 +0200

    switched from tslint to eslint and added prettier

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,20 +1,21 @@
-import winston from 'winston';
+import winston from 'winston'
 
-const Logger = winston.createLogger(
-    {
-        level: 'info',
-        format: winston.format.combine(
-            winston.format.timestamp({
-                format: 'YYYY-MM-DD HH:mm:ss'
-            }),
-            winston.format.printf((info) => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`),
-            winston.format.colorize({ all: true })
+const Logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        winston.format.printf(
+            (info) =>
+                `${info.timestamp} ${info.level.toUpperCase()}: ${
+                    info.message
+                }`,
         ),
-        // available log levels: error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5
-        transports: [
-            new winston.transports.Console()
-        ]
-    }
-);
+        winston.format.colorize({ all: true }),
+    ),
+    // available log levels: error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5
+    transports: [new winston.transports.Console()],
+})
 
-export default Logger;
+export default Logger
