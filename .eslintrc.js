6dd5ff41343dd761587b98e34d7a49bb74472fb8
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2021-08-04 16:02:19 +0200

    [prettier] formating semicolons back in

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,4 +12,4 @@ module.exports = {
         'prettier',
         'plugin:prettier/recommended',
     ],
-}
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express'
-import helmet from 'helmet'
+import express from 'express';
+import helmet from 'helmet';
 
-import Logger from './utils/logger'
+import Logger from './utils/logger';
 
-const app = express()
-const staticPath = __dirname + '/public'
-const defaultPort = 3000
-const port = process.env.port || defaultPort
+const app = express();
+const staticPath = __dirname + '/public';
+const defaultPort = 3000;
+const port = process.env.port || defaultPort;
 
 // express server basic security
 app.use(
@@ -18,25 +18,28 @@ app.use(
             },
         },
     }),
-)
+);
 
 // express server path configuration
-app.use(express.static(staticPath))
+app.use(express.static(staticPath));
 app.use((req, res) => {
     // 404 handling
-    Logger.log('warn', `Returning 404 for Request: ${req.method} : ${req.path}`)
-    res.status(404).sendFile(staticPath + '/404.html')
-})
+    Logger.log(
+        'warn',
+        `Returning 404 for Request: ${req.method} : ${req.path}`,
+    );
+    res.status(404).sendFile(staticPath + '/404.html');
+});
 
 // starting the server
 const server = app.listen(port, () => {
-    Logger.log('info', `Server started at http://localhost:${port}`)
-})
+    Logger.log('info', `Server started at http://localhost:${port}`);
+});
 
 // stopping the server correctly
 const closeGracefully = async () => {
-    await server.close()
-    Logger.log('info', `Server closed.`)
-    process.exit()
-}
-process.on('SIGINT', closeGracefully)
+    await server.close();
+    Logger.log('info', `Server closed.`);
+    process.exit();
+};
+process.on('SIGINT', closeGracefully);
