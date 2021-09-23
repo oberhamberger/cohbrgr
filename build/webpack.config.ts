6a56b5ff3ce5c246b80c71e280aee670cf1c1ac1
Christian Oberhamberger <c.oberhamberger@gmail.com>
2021-09-23 13:39:57 +0200

    refactored webpack configs

--- build/webpack.config.ts
+++ build/webpack.config.ts
@@ -1,14 +1,7 @@
-import { Configuration } from 'webpack';
-import WebpackBar from 'webpackbar';
-import HtmlMinimizerPlugin from 'html-minimizer-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
-import ESLintPlugin from 'eslint-webpack-plugin';
-
 import { join, resolve, dirname } from 'path';
-
-// in case you run into any typescript error when configuring `devServer`
-
-const CWD = process.cwd();
+import { Configuration } from 'webpack';
+import webpackClientConfig from './webpack.client.config';
+import webpackServerConfig from './webpack.server.config';
 
 export enum Mode {
     DEVELOPMENT = 'development',
@@ -18,96 +11,4 @@ export enum Mode {
 const isProduction = process.env.NODE_ENV === Mode.PRODUCTION;
 
 
-const clientConfig: Configuration = {
-  mode: isProduction ? Mode.PRODUCTION : Mode.DEVELOPMENT,
-  devtool: isProduction ? false : 'inline-source-map',
-  entry: {},
-  resolve: {
-    extensions: ['.html', '.json', '.js'],
-    modules: [
-        join(CWD, ''),
-        join(CWD, 'node_modules'),
-        join(dirname(require.main!.filename), '..', 'node_modules'),
-        join(dirname(require.main!.filename), 'node_modules'),
-        'node_modules',
-    ],
-    alias: { src: 'src' },
-},
-  plugins: [
-    new ESLintPlugin(),
-    new WebpackBar({
-      name: 'Client',
-      color: 'aquamarine'
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "./src/client",
-          to: "./client/"
-        },
-      ],
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new HtmlMinimizerPlugin(),
-    ],
-  },
-  output: {
-      path: resolve(__dirname, '../dist/'),
-      clean: true,
-  },
-}
-
-export const serverConfig: Configuration = {
-    mode: isProduction ? Mode.PRODUCTION : Mode.DEVELOPMENT,
-    devtool: isProduction ? false : 'inline-source-map',
-    target: 'node',
-    entry: {
-        server: resolve(__dirname, '../src/server'),
-    },
-    module: {
-        rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        modules: [
-            join(CWD, ''),
-            join(CWD, 'node_modules'),
-            join(dirname(require.main!.filename), '..', 'node_modules'),
-            join(dirname(require.main!.filename), 'node_modules'),
-            'node_modules',
-        ],
-        alias: { src: 'src' },
-    },
-    plugins: [
-      new WebpackBar({
-        name: 'Server',
-        color: '#ff50e1'
-      }),
-    ],
-    output: {
-        path: resolve(__dirname, '../dist/server/'),
-        filename: 'index.js',
-        clean: true,
-        publicPath: '/',
-    },
-    
-};
-
-
-export default [clientConfig, serverConfig];
-
-
-
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-
+export default [webpackClientConfig(isProduction), webpackServerConfig(isProduction)];
---
+++ build/webpack.server.config.ts
@@ -0,0 +1,49 @@
+import { Configuration } from 'webpack';
+import WebpackBar from 'webpackbar';
+import { join, resolve, dirname } from 'path';
+
+import { Mode } from './webpack.config';
+const CWD = process.cwd();
+
+export default (isProduction: boolean): Configuration => ({
+    mode: isProduction ? Mode.PRODUCTION : Mode.DEVELOPMENT,
+    devtool: isProduction ? false : 'inline-source-map',
+    target: 'node',
+    entry: {
+        server: resolve(__dirname, '../src/server'),
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        modules: [
+            join(CWD, ''),
+            join(CWD, 'node_modules'),
+            join(dirname(require.main!.filename), '..', 'node_modules'),
+            join(dirname(require.main!.filename), 'node_modules'),
+            'node_modules',
+        ],
+        alias: { src: 'src' },
+    },
+    plugins: [
+      new WebpackBar({
+        name: 'Server',
+        color: '#ff50e1'
+      }),
+    ],
+    output: {
+        path: resolve(__dirname, '../dist/server/'),
+        filename: 'index.js',
+        clean: true,
+        publicPath: '/',
+    },
+    
+});
