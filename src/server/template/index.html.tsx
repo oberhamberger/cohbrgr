1d9a2c546ee5478729b4ab12e5b464ef0d98585d
Christian Oberhamberger <c.oberhamberger@gmail.com>
2021-10-04 15:48:24 +0200

    updated favicons (again)

--- src/server/template/index.html.tsx
+++ src/server/template/index.html.tsx
@@ -43,9 +43,23 @@ const Index: FunctionComponent<IIndexProps> = (props: IIndexProps) => {
                 <link rel="canonical" href="https://cohbrgr.com/" />
                 <title>Christian Oberhamberger</title>
 
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
 
                 <link rel="manifest" href="/manifest.json" />
 
