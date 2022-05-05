08ba1092bfaa1d42f40fef73cf157b4eeda0423f
Christian Oberhamberger <c.oberhamberger@gmail.com>
2022-05-05 13:29:11 +0200

    update of types to react18

--- src/client/components/layout/index.tsx
+++ src/client/components/layout/index.tsx
@@ -1,7 +1,11 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, ReactNode } from 'react';
 import styles from 'src/client/components/layout/layout.module.scss';
 
-const Layout: FunctionComponent = ({ children }) => {
+interface ILayout {
+    children: ReactNode;
+}
+
+const Layout: FunctionComponent<ILayout> = ({ children }) => {
     return <div className={styles.layout}>{children}</div>;
 };
 
