b675b66ad1d4fa7ddf57abb80b890ee6998aee7c
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2022-12-22 19:43:55 +0100

    changed deprecated type in jest.config

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -1,14 +1,14 @@
-import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+import type { JestConfigWithTsJest } from 'ts-jest/dist/types';
 
 export const moduleNames = {
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
         'jest-transform-stub',
 };
 
-const config: InitialOptionsTsJest = {
+const config: JestConfigWithTsJest = {
     preset: 'ts-jest/presets/default-esm',
     transform: {
-        '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true,}],
+        '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
     },
 };
 
