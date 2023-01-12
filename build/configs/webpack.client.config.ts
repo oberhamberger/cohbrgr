6c3d13ece0cc105fbf33d8dc9f28971b9e45f6da
Christian Oberhamberger <c.oberhamberger@gmail.com>
2023-01-12 19:32:37 +0100

    webpack client build reformating

--- build/configs/webpack.client.config.ts
+++ build/configs/webpack.client.config.ts
@@ -8,111 +8,99 @@ import { InjectManifest } from 'workbox-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import {
-    Mode,
-    isProduction,
-    regexStyle,
-    regexSource,
-    CWD,
-    serviceWorker,
+Mode,
+isProduction,
+regexStyle,
+regexSource,
+CWD,
+serviceWorker,
 } from '../utils/constants';
 import getStyleLoader from '../loader/style.loader';
 
-const getWebpackClientConfig = (): Configuration => {
-    const clientConfig: Configuration = {
-        mode: isProduction ? Mode.PRODUCTION : Mode.DEVELOPMENT,
-        devtool: isProduction ? false : 'inline-source-map',
-        context: resolve(__dirname, 'src'),
-        entry: {
-            client: 'src/client',
-        },
-        target: 'web',
-        module: {
-            rules: [
-                {
-                    test: regexSource,
-                    loader: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-                {
-                    test: regexStyle,
-                    use: getStyleLoader(false, isProduction),
-                },
-            ],
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js', '.scss'],
-            modules: [
-                join(CWD, ''),
-                join(CWD, 'node_modules'),
-                join(
-                    dirname(require.main?.filename || ''),
-                    '..',
-                    'node_modules',
-                ),
-                join(dirname(require.main?.filename || ''), 'node_modules'),
-                'node_modules',
-                'node_modules',
-            ],
-            alias: { src: 'src/' },
-        },
-        plugins: [
-            new ESLintPlugin(),
-            new WebpackBar({
-                name: 'Client',
-                color: '#fff1ee',
-            }),
-            new MiniCssExtractPlugin({
-                filename: isProduction
-                    ? 'css/[id].[contenthash].css'
-                    : 'css/[name].css',
-            }),
-            new CopyPlugin({
-                patterns: [{ from: '../../../src/assets/static', to: './' }],
-            }),
-            isProduction
-                ? new InjectManifest({
-                      swSrc: 'src/service-worker',
-                      swDest: serviceWorker,
-                      include: [/\.js$/],
-                  })
-                : () => null,
+const isAnalyze = process.env.ANALYZE === 'true';
+
+export default (): Configuration => ({
+    mode: isProduction ? Mode.PRODUCTION : Mode.DEVELOPMENT,
+    devtool: isProduction ? false : 'inline-source-map',
+    context: resolve(__dirname, 'src'),
+    entry: {
+        client: 'src/client',
+    },
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: regexSource,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: regexStyle,
+                use: getStyleLoader(false, isProduction),
+            },
         ],
-        optimization: {
-            chunkIds: isProduction ? 'natural' : 'named',
-            minimize: isProduction,
-            splitChunks: {
-                cacheGroups: {
-                    vendor: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'vendor',
-                        chunks: 'all',
-                    },
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.scss'],
+        modules: [
+            join(CWD, ''),
+            join(CWD, 'node_modules'),
+            join(
+                dirname(require.main?.filename || ''),
+                '..',
+                'node_modules',
+            ),
+            join(dirname(require.main?.filename || ''), 'node_modules'),
+            'node_modules',
+            'node_modules',
+        ],
+        alias: { src: 'src/' },
+    },
+    plugins: [
+        new ESLintPlugin(),
+        new WebpackBar({
+            name: 'Client',
+            color: '#fff1ee',
+        }),
+        new MiniCssExtractPlugin({
+            filename: isProduction
+                ? 'css/[id].[contenthash].css'
+                : 'css/[name].css',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: '../../../src/assets/static', to: './' }],
+        }),
+        isProduction
+            ? new InjectManifest({
+                    swSrc: 'src/service-worker',
+                    swDest: serviceWorker,
+                    include: [/\.js$/],
+                })
+            : () => null,
+        isAnalyze 
+            ? new BundleAnalyzerPlugin({
+                generateStatsFile: true,
+                openAnalyzer: true,
+                analyzerPort: 0,
+            }) : () => null
+    ],
+    optimization: {
+        chunkIds: isProduction ? 'natural' : 'named',
+        minimize: isProduction,
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all',
                 },
             },
         },
-        output: {
-            path: resolve(__dirname, '../../dist/client'),
-            filename: isProduction
-                ? 'js/[id].[contenthash].js'
-                : 'js/[name].js',
-        },
-    };
-
-    const isAnalyze = process.env.ANALYZE === 'true';
-    if (isAnalyze) {
-        const analyzerPlugin = new BundleAnalyzerPlugin({
-            generateStatsFile: true,
-            openAnalyzer: true,
-            analyzerPort: 0,
-        });
-        if (clientConfig.plugins) {
-            clientConfig.plugins.unshift(analyzerPlugin);
-        } else {
-            clientConfig.plugins = [analyzerPlugin];
-        }
-    }
-
-    return clientConfig;
-};
-
-export default getWebpackClientConfig;
+    },
+    output: {
+        path: resolve(__dirname, '../../dist/client'),
+        filename: isProduction
+            ? 'js/[id].[contenthash].js'
+            : 'js/[name].js',
+    },
+});
