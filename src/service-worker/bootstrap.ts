4382ca5c6d66bb207bdcf949440e43a476c45720
Christian Oberhamberger <c.oberhamberger@gmail.com>
2023-01-13 14:37:36 +0100

    another one

--- src/service-worker/bootstrap.ts
+++ src/service-worker/bootstrap.ts
@@ -1,9 +1,8 @@
-import { Workbox } from 'workbox-window';
-
 const serviceWorker = '/sw.js';
 
 const registerServiceWorker = async () => {
     if ('serviceWorker' in navigator) {
+        const { Workbox } = await import('workbox-window');
         const workbox = new Workbox(serviceWorker);
         workbox.register();
     }
