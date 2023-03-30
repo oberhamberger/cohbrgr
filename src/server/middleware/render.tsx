d62cb813e8b11c3772438dc9bf970bf57a5e4e4c
Christian Oberhamberger <c.oberhamberger@gmail.com>
2023-03-30 16:50:32 +0200

    draft basic renderToPipeableStream implementation

--- src/server/middleware/render.tsx
+++ src/server/middleware/render.tsx
@@ -1,19 +1,16 @@
 import { Request, Response } from 'express';
-import { renderToString } from 'react-dom/server';
+import { renderToPipeableStream } from 'react-dom/server';
 
 import Index from 'src/server/template/Index.html';
 import Logger from 'src/server/utils/logger';
 import { HttpContextData } from 'src/client/contexts/http';
-import { HttpMethod } from 'src/server/middleware/methodDetermination';
-
-const doctype = '<!DOCTYPE html>';
 
 const render =
     (isProduction: boolean, useClientSideRendering: boolean) =>
     async (req: Request, res: Response) => {
         const httpContext: HttpContextData = {};
 
-        const markup = await renderToString(
+        const stream = renderToPipeableStream(
             <Index
                 isProduction={isProduction}
                 location={req.url}
@@ -21,28 +18,26 @@ const render =
                 nonce={res.locals.nonce}
                 httpContextData={httpContext}
             />,
+            {
+                onShellReady: () => {
+                    const renderStatusCode = httpContext.statusCode || 200;
+                    if (renderStatusCode < 300) {
+                        Logger.info(`Rendered App with path: ${req.url}`);
+                    } else if (renderStatusCode < 400) {
+                        Logger.warn(`Redirected: ${req.url}`);
+                    } else if (renderStatusCode < 500) {
+                        Logger.warn(`Not found: ${req.url}`);
+                    } else {
+                        Logger.error(`Major Server Error while rendering: ${req.url}`);
+                    }
+
+                    res.status(renderStatusCode);
+                    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+                },
+            },
         );
 
-        const renderStatusCode = httpContext.statusCode || 200;
-
-        if (renderStatusCode < 300) {
-            Logger.info(`Rendered App with path: ${req.url}`);
-        } else if (renderStatusCode < 400) {
-            Logger.warn(`Redirected: ${req.url}`);
-        } else if (renderStatusCode < 500) {
-            Logger.warn(`Not found: ${req.url}`);
-        } else {
-            Logger.error(`Major Server Error while rendering: ${req.url}`);
-        }
-
-        res.status(renderStatusCode);
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        if (req.method === HttpMethod.GET) {
-            res.send(doctype + markup);
-        } else if (req.method === HttpMethod.HEAD) {
-            res.send();
-        }
-        res.end();
+        stream.pipe(res);
     };
 
 export default render;
