f4478a9488c56dd907a4c0b371a7380a5cf5d7de
Christian Oberhamberger <c.oberhamberger@gmail.com>
2024-02-19 21:05:59 +0100

    removed unused package for now

--- apps/content/src/server/utils/findProcessArgs.ts
+++
@@ -1,7 +0,0 @@
-export const findProcessArgs = (searchArgs: string | string[]): boolean => {
-    return (
-        process.argv.filter(
-            (arg) => arg === searchArgs || searchArgs.toString().includes(arg),
-        ).length > 0
-    );
-};
