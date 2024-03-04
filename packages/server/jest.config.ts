6c927d31fcec120236f6d47ac8e2d28db96669ae
Christian Oberhamberger <c.oberhamberger@gmail.com>
2024-03-04 21:30:59 +0100

    created server module

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -5,12 +5,14 @@ const config: JestConfigWithTsJest = {
     ...baseConfig,
     displayName: 'server',
     testEnvironment: 'node',
-    rootDir: './../../',
+    rootDir: '.',
     modulePaths: ['node_modules', '<rootDir>'],
     testMatch: [
-        '**/server/**/__tests__/**/*.+(ts|tsx|js)',
-        '**/server/**/?(*.)+(spec|test).+(ts|tsx|js)',
+        '**/__tests__/**/*.+(ts|tsx|js)',
+        '**/?(*.)+(spec|test).+(ts|tsx|js)',
     ],
 };
 
-export default config;
+export default {
+    projects: [config],
+};
