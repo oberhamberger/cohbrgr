5811a081ea80ec15b2a5e505411e2e54ffb63bfd
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2024-04-07 21:08:39 +0200

    ran prettier

--- packages/environments/index.ts
+++ packages/environments/index.ts
@@ -6,37 +6,45 @@ const Config = {
         port: 3000,
         location: {
             local: 'http://localhost',
-            production: 'https://cohbrgr-shell-o44imzpega-oa.a.run.app/'
+            production: 'https://cohbrgr-shell-o44imzpega-oa.a.run.app/',
         },
         staticPath: {
             local: '/dist',
-            production: 'apps/shell/dist'
-        }
+            production: 'apps/shell/dist',
+        },
     },
     content: {
         port: 3001,
         location: {
             local: 'http://localhost',
-            production: 'https://cohbrgr-content-o44imzpega-oa.a.run.app/'
+            production: 'https://cohbrgr-content-o44imzpega-oa.a.run.app/',
         },
         staticPath: {
             local: '/dist',
-            production: 'apps/content/dist'
-        }
+            production: 'apps/content/dist',
+        },
     },
 };
 
 export const EnvironmentConfig = {
     shell: {
         port: Config.shell.port,
-        location: isDocker ? Config.shell.location.production : Config.shell.location.local,
-        staticPath: isDocker ? Config.shell.staticPath.production : Config.shell.staticPath.local
+        location: isDocker
+            ? Config.shell.location.production
+            : Config.shell.location.local,
+        staticPath: isDocker
+            ? Config.shell.staticPath.production
+            : Config.shell.staticPath.local,
     },
     content: {
         port: Config.content.port,
-        location: isDocker ? Config.content.location.production : Config.content.location.local,
-        staticPath: isDocker ? Config.content.staticPath.production : Config.content.staticPath.local
-    }
-}
+        location: isDocker
+            ? Config.content.location.production
+            : Config.content.location.local,
+        staticPath: isDocker
+            ? Config.content.staticPath.production
+            : Config.content.staticPath.local,
+    },
+};
 
 export default EnvironmentConfig;
