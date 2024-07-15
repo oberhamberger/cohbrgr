3887a51703b60704007e41b0f0349f490d40ee09
Christian Oberhamberger <c.oberhamberger@gmail.com>
2024-07-15 11:03:59 +0200

    fixed eslint config

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,9 +1,9 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export const config = tseslint.config(
+export const config = tseslint.config([
     eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-);
+    tseslint.configs.recommended,
+]);
 
 export default config;
