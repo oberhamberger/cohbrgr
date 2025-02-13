4177f9bebbb3acf1e737fc4d35178c957a9b5091
Christian Oberhamberger <c.oberhamberger@gmail.com>
2025-02-13 19:58:25 +0100

    removed build from build package

--- apps/content/rspack.config.ts
+++ apps/content/rspack.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from '@rspack/cli';
-import build from '@cohbrgr/build';
+import { getRsPackNodeConfig } from '@cohbrgr/build';
 
-export default defineConfig(build.getRsPackNodeConfig());
+export default defineConfig(getRsPackNodeConfig());
