1ce1eab90bcd638931ecab53dfe99671d5f4d495
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2025-02-26 17:27:06 +0100

    feat(content): header redesign progress

--- apps/content/src/client/components/content/Content.tsx
+++ apps/content/src/client/components/content/Content.tsx
@@ -10,8 +10,8 @@ const Content: FunctionComponent<IContent> = ({ nonce }) => {
     return (
         <>
             <main>
-                <h1>Hi!</h1>
-                <h2>My name is Christian</h2>
+                <h5>C.F.K.O</h5>
+                <h1>My name is Christian</h1>
                 <p>
                     I am a Frontend Developer at{' '}
                     <a href="https://www.netconomy.net/">Netconomy</a>. I mainly
