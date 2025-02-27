facf96964190fa8e876cdd5eb1b926a2f118a21b
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2025-02-27 14:06:43 +0100

    feat(content): updated colors and content

--- apps/content/src/client/components/content/Content.tsx
+++ apps/content/src/client/components/content/Content.tsx
@@ -10,7 +10,7 @@ const Content: FunctionComponent<IContent> = ({ nonce }) => {
     return (
         <>
             <main>
-                <h5>C.F.K.O</h5>
+                <p role="doc-subtitle">C.F.K.O<br/></p>
                 <h1>My name is Christian</h1>
                 <p>
                     I am a Frontend Developer at{' '}
