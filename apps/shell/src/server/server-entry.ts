bd7fbd2dd0e4d8adb4a2715a0c1ee30b2f580d02
Christian Oberhamberger <c.oberhamberger@gmail.com>
2025-04-11 21:22:13 +0200

    chore: ran prettier

--- apps/shell/src/server/server-entry.ts
+++ apps/shell/src/server/server-entry.ts
@@ -2,7 +2,8 @@ import type { Request, Response } from 'express';
 
 const createRenderThunk = () => async (req: Request, res: Response) => {
     // @ts-expect-error
-    const renderer = (await import('./middleware/render')).default as unknown as RenderMiddlewareFactory;
+    const renderer = (await import('./middleware/render'))
+        .default as unknown as RenderMiddlewareFactory;
     return renderer(true, true)(req, res);
 };
 
