89a22664084cc90c1c024f29d0e5e29687775295
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2025-11-14 13:52:38 +0100

    feat: added health routes to server package and used in api

--- packages/server/src/middleware/error.ts
+++ packages/server/src/middleware/error.ts
@@ -1,4 +1,9 @@
-import type { ErrorRequestHandler, Request, Response } from 'express';
+import type {
+    ErrorRequestHandler,
+    NextFunction,
+    Request,
+    Response,
+} from 'express';
 
 import { Logger } from '@cohbrgr/utils';
 
@@ -6,6 +11,8 @@ export const errorHandler: ErrorRequestHandler = (
     err,
     req: Request,
     res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _next: NextFunction,
 ) => {
     Logger.error(`error: ${req.method} ${req.url} ${err}`);
 
