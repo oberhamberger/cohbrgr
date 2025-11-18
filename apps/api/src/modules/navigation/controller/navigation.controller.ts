b0da96a9c098a0586187e14db5d8820f286cb23c
Christian Oberhamberger <c.oberhamberger@gmail.com>
2025-11-18 21:49:59 +0100

    feat(api): added proper navigation routes

--- apps/api/src/modules/navigation/controller/navigation.controller.ts
+++ apps/api/src/modules/navigation/controller/navigation.controller.ts
@@ -6,5 +6,25 @@ export const fullNavigationController = (
     _request: Request,
     response: Response,
 ) => {
-    sendJsonWithEtag(response, navigationService.get());
+    return sendJsonWithEtag(response, navigationService.get());
+};
+
+export const subNavigationController = (
+    _request: Request,
+    response: Response,
+) => {
+    const { nodeId } = _request.params;
+
+    if (!nodeId) {
+        return response.status(400).json({ error: 'Node ID is required' });
+    }
+
+    const subNavigation = navigationService.getSubNavigation(nodeId);
+    if (subNavigation) {
+        return sendJsonWithEtag(response, subNavigation);
+    } else {
+        return response
+            .status(404)
+            .json({ error: `Sub-navigation for '${nodeId}' not found` });
+    }
 };
