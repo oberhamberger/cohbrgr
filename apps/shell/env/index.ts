2d8239fa2836a76202ab76c8086bb9976530dd26
Christian Oberhamberger <c.oberhamberger@netconomy.net>
2026-01-08 13:19:25 +0100

    chore: added express dependency to all apps and server package explicitely

--- apps/shell/env/index.ts
+++ apps/shell/env/index.ts
@@ -7,7 +7,7 @@ const internalConfig = {
     docker: {
         port: 3000,
         location: 'https://cohbrgr-o44imzpega-oa.a.run.app/',
-        staticPath: '/apps/shell/dist',
+        staticPath: '/dist',
     },
 };
 
